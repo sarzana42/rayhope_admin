87f2ae1851aa15e66429548c8f475733bc21c068
sarzana42 <sarzana42@gmail.com>
2016-09-11 09:01:33 +0000

    gravatar error

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:info] = "{@user.name}として、ログインしました。"
+      redirect_to @user
+    else
+      flash[:danger] = "メールアドレスかパスワードが一致しません"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+  
+end
